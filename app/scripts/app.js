d0bf9386a2b3050daef5ca866f74913ede10f1f8
Gregg Whitworth <whitwort@gmail.com>
2014-01-30 20:43:45 -0500

    Bower update of client libs; update of shiny app urls to shiny-apps-nows.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,4 +3,6 @@ define([], function () {
     'use strict';
     
     return 'Welcome to RNA!  If you are a curious enough soul to have opened the console for this web app, perhaps you would also enjoy seeing the un-minified source at http://github.com/whitwort/rna-wlu-edu.';
+    
+    
 });
